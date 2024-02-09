f6b8d41b9361c94d882df942572824aaa214d9c4
mohiniu4 <110068413+mohiniu4@users.noreply.github.com>
2024-02-09 18:08:08 -0500

    Add files via upload
    Finished requirement 3

--- repository4/Assignment1/src/Main.java
+++ repository4/Assignment1/src/Main.java
@@ -1,9 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Main {
+	@SuppressWarnings("unused")
+	public static void Main(String[] args) {
+		
+		// Declaration and assignment
+		float x;
+		float y;
+		float z;
+		
+		int Orientation;
+		
+		String Orientation1 = "Face up";
+		String Orientation2 = "Face down";
+		String Orientation3 = "Portrait";
+		String Orientation4 = "Portrait upside down";
+		String Orientation5 = "Landscape left";
+		String Orientation6 = "Landscape Right";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Team!");
+
+		// opening the training file
+		String filePath = "C:\\Users\\Umang\\Downloads\\trainingData1.txt"; // this will have to vary person running the code
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+		    String line;
+
+			// Loop until the end of the file
+		    while ((line = reader.readLine()) != null) { 
+
+				// Separates values by commas
+		        String[] values = line.split(","); 
+		        
+		        // makes sure each line has 4 values
+		        if (values.length >= 4) {
+
+		            // reads the values and assigns them to a variable
+		            x = Float.parseFloat(values[0]);
+		            y = Float.parseFloat(values[1]);
+		            z = Float.parseFloat(values[2]);
+		            Orientation = Integer.parseInt(values[3]);
+		            
+		            switch(Orientation) {
+		            case 1:
+		            	System.out.println("Orientation: " + Orientation1);
+		            	break;
+		            case 2:
+		            	System.out.println("Orientation: " + Orientation2);
+		            	break;
+		            case 3:
+		            	System.out.println("Orientation: " + Orientation3);
+		            	break;
+		            case 4:
+			            System.out.println("Orientation: " + Orientation4);
+			            break;
+		            case 5:
+		            	System.out.println("Orientation: " + Orientation5);
+		            	break;
+		            case 6:
+		            	System.out.println("Orientation: " + Orientation6);
+		            	break;
+		            }
+		        }
+		    }
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	
+		
 	}
-
+	
+	
 }
