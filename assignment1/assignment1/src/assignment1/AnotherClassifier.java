2945edbfc0762cc6d5a6f65f941a9413f5fca5e5
Osezuah <122998658+Osezuah@users.noreply.github.com>
2024-02-12 11:03:52 -0500

    Add files via upload

---
+++ assignment1/assignment1/src/assignment1/AnotherClassifier.java
@@ -0,0 +1,11 @@
+package assignment1;
+
+public class AnotherClassifier extends NNClassifier{
+	@Override
+	public void printNN()
+	{
+		System.out.println("PrintNN function is called");
+	}
+	
+
+}
---
+++ assignment1/assignment1/src/assignment1/Main.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class Main {
+	public static void main(String[] args) {
+    	AnotherClassifier AC = new AnotherClassifier();
+    	AC.printNN();
+        //NNClassifier a = new NNClassifier();
+       // a.printNN(); // Prints: Hello from ClassA
+
+       // KNNClassifier b = new KNNClassifier();
+      //  b.printMessage(); // Prints: Hello from ClassB
+    }
+}
